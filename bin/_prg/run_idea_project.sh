738290fa8dd8b50d5bdc88183b46609959834323
wellic <wellic72@gmail.com>
2025-08-31 15:41:00 +0300

    feat: improve Bash scripts and network handling
    - Added `_fix_vpn` script to clean up netplan configurations and restart the network service.
    - Enhanced `_check_speedtest` script with sorted interface selection.
    - Improved `run_idea_project.sh` script with realpath support and user-friendly menu for project handling.
    - Renamed `_check_cert_hostname` for better script organization.

--- bin/_prg/run_idea_project.sh
+++ bin/_prg/run_idea_project.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 set -uEeo pipefail
-set -x
+#set -x
 
 ide="$1"
 shift
 
-START_DIR="$(pwd)"
+START_DIR="$(realpath "$(pwd)")"
 dir="$START_DIR"
 [[ $dir =~ "$HOME" ]] || exit 111
 
@@ -19,16 +19,42 @@ done
 while [[ "$dir" != "$HOME" ]] && [[ "$dir" != "/" ]]; do
   if [[ -d "$dir/.idea" ]]; then
     echo "Opening project in: $dir"
-    nohup $ide "$dir" ${files[@]} >/dev/null 2>&1 &
+    nohup $ide "$(realpath "$dir")" ${files[@]} >/dev/null 2>&1 &
     exit 0
   fi
   dir=$(dirname "$dir")
 done
 
+
 #if (( ${#files[@]} > 0 )); then
-    nohup $ide --temp-project ${files[@]} >/dev/null 2>&1 &
+#    nohup $ide --temp-project ${files[@]} >/dev/null 2>&1 &
 #else
 #    nohup $ide --temp-project $START_DIR  >/dev/null 2>&1 &
 #fi
 
-echo "No $ide project (.idea) found up to $START_DIR"
+menu=(
+  "Only open $ide"
+  "Create project"
+)
+
+echo "No $ide project (.idea) found in: $START_DIR"
+PS3="Enter number or type 'q' to quit: "
+
+select opt in "${menu[@]}"; do
+  [[ "$REPLY" =~ ^[qQ] ]] && exit 0
+  case $REPLY in
+    1)
+#      nohup $ide --temp-project "$START_DIR" ${files[@]} >/dev/null 2>&1 &
+      nohup $ide ${files[@]} >/dev/null 2>&1 &
+      exit 0
+      ;;
+    2)
+      nohup $ide "$START_DIR" ${files[@]} >/dev/null 2>&1 &
+      exit 0
+      ;;
+    *) 
+      echo "Invalid selection."
+      exit 1
+      ;;
+  esac
+done
