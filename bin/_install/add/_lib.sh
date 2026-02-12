280e490d4065a25f369a0ca2cff5f54353ddcf9d
wellic <wellic72@gmail.com>
2026-02-12 22:46:27 +0200

    feat(bin): refactor library sourcing and add `_vars.sh`
    - Centralized `LIB_DIR` initialization in `_lib.sh` for consistent path management.
    - Added new `_vars.sh` for managing shared script variables.
    - Updated `_install_gh.sh` to source `_vars.sh` and enhance logging with repo and tool details.
    - Removed redundant variable declarations from `_install_gh.sh`.

--- bin/_install/add/_lib.sh
+++ bin/_install/add/_lib.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+LIB_DIR="$SCRIPT_DIR/../lib"
 #shellcheck source=../lib/_install_gh.sh
-source "$SCRIPT_DIR/../lib/_install_gh.sh"
+source "${LIB_DIR}/_install_gh.sh"
--- bin/_install/lib/_install_gh.sh
+++ bin/_install/lib/_install_gh.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+source "$LIB_DIR/_vars.sh"
 #set -ueo pipefail
 set -u
 
@@ -108,6 +109,9 @@ EOF
 }
 
 _main() {
+  echo "Repo: https://github.com/$repo/"
+  echo "Tool: $tool_name"
+  echo
   local version_current
 #set -x
     url_releases=$(get_url_releases)
@@ -159,36 +163,3 @@ show_completion() {
 EOF
   fi
 }
-
-########################
-# curl | wget
-DOWNLOADER=curl
-
-tmp_dir=/tmp
-DST_BIN_DIR=/usr/local/bin
-tool_name=""
-
-def_mask=browser_download_url
-def_mask=\/download\/
-def_mask_deb=${def_mask}.*.deb
-def_mask_amd64_deb=${def_mask}.*amd64.deb
-#def_mask_bin=${def_mask}'.*[_-][Ll]inux[_-]amd64"'
-#def_mask_bin_x64=${def_mask}'.*[_-][Ll]inux[_-]x64"'
-def_mask_bin=${def_mask}'.*[_-][Ll]inux[_-]amd64'
-def_mask_bin_x64=${def_mask}'.*[_-][Ll]inux[_-]x64'
-def_mask_amd64_bz2=${def_mask}.*linux_amd64.bz2
-def_mask_x86_64_tar_gz=${def_mask}.*[Ll]inux[_-]x86_64.tar.gz
-def_mask_amd64_tar_gz=${def_mask}.*[Ll]inux[_-]amd64.tar.gz
-
-OPT_VERSION=--version
-COMPLETION_OPT=completion
-COMPLETION_LNG=bash
-
-last_releases=10
-
-before_install_cmd=()
-after_install_cmd=()
-
-show_completion=1
-
-tar_strip_components=1
